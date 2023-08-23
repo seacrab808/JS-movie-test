e08f9d9cb14ce12216736ee3c40805972a8e6adf
seacrab808 <seacrab808@naver.com>
2023-08-23 12:29:36 +0900

    20230823 강의 07까지

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { Component } from "./core/heropy";
+import TheHeader from "./components/TheHeader";
+
+export default class App extends Component {
+  render() {
+    const routerView = document.createElement("router-view");
+    this.el.append(new TheHeader().el, routerView);
+  }
+}
